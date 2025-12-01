f3a50ecb6a98ef4a3f55225058f242260879f8a0
miguel2015sandoval2015-blip <miguel2015sandoval2015@gmail.com>
2025-12-01 08:35:19 -0500

    Add files via upload

---
+++ ejercico_calcular_area_triangulo.py
@@ -0,0 +1,22 @@
+#*********** zonas de funciones *****************
+def definir_altura():
+    altura = 15
+    return altura
+def definir_base():
+    base = 25
+    return base
+def calcular_area(base, altura):
+    area = (base * altura)/2
+    return area
+def imprimir_datos(base, altura):
+    mensaje = "la base es: " + base
+    mensaje = "la altura es: " + altura
+def imprimir_resultado(resultado_area):
+    print("el area del triangulo es: " + str(resultado_area))
+
+#********** codigo principal *******************
+
+base = definir_base()
+altura =definir_altura()
+area = calcular_area(base, altura)
+resultado = imprimir_resultado(area)
