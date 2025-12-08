59acebb7f7019fc460aebb82da6cd163706dcae1
miguel2015sandoval2015-blip <miguel2015sandoval2015@gmail.com>
2025-12-08 10:13:24 -0500

    Add files via upload

---
+++ ejercicio25_division.py
@@ -0,0 +1,25 @@
+#******** zona de fucniones ******************
+def capturar_numero():
+    numero = int(input("digitar el numero a dividir"))
+    return numero
+
+def capturar_divisor():
+    divisor = int(input("digite el divisor: "))
+    return divisor
+
+def procesar_datos(numero,divisor):
+    cociente = numero/divisor
+    return cociente
+
+def mostrar_resultado(cociente):
+    print("el resultado de la divion es: ", cociente)
+    
+def chao_con_adios():
+    print("el programa a finalizado")
+    
+#***** zona de codigo principal ***************
+numero = capturar_numero()
+divisor = capturar_divisor()
+cociente = procesar_datos(numero,divisor)
+resultado = mostrar_resultado(cociente)
+despedida = chao_con_adios()        
