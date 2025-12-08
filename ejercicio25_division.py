7f18229434d93ef93a62b032b1bc46239ce2003d
miguel2015sandoval2015-blip <miguel2015sandoval2015@gmail.com>
2025-12-08 10:18:38 -0500

    Change input types to float for division

--- ejercicio25_division.py
+++ ejercicio25_division.py
@@ -1,10 +1,10 @@
 #******** zona de fucniones ******************
 def capturar_numero():
-    numero = int(input("digitar el numero a dividir"))
+    numero = float(input("digitar el numero a dividir"))
     return numero
 
 def capturar_divisor():
-    divisor = int(input("digite el divisor: "))
+    divisor = float(input("digite el divisor: "))
     return divisor
 
 def procesar_datos(numero,divisor):
@@ -23,3 +23,4 @@ divisor = capturar_divisor()
 cociente = procesar_datos(numero,divisor)
 resultado = mostrar_resultado(cociente)
 despedida = chao_con_adios()        
+
